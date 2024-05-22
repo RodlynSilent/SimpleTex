03ade2c09091ce683532df3804ee5350bc4eac6d
Rodlyn Mahilum <123444084+RodlynSilent@users.noreply.github.com>
2024-05-22 21:20:42 +0800

    finalizing the design ^_^

--- simpletex.py
+++ simpletex.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
 from PIL import Image
+import base64
+
+st.set_page_config(page_title="SimpleTex: Your Simple Keyword Extractor")
 
 def extract_keywords(text):
+    
     # Create a TF-IDF vectorizer
     tfidf = TfidfVectorizer(stop_words='english')
 
@@ -27,39 +30,84 @@ def extract_keywords(text):
 
     return top_keywords
 
+def load_image(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+    return encoded_string
+
 # Load the logo image
 logo_path = "simpletex_logo.png"
-logo = Image.open(logo_path)
+logo_base64 = load_image(logo_path)
 
-# Set up the Streamlit interface
-st.image(logo, width=150)  # Resize the logo to a smaller width
-st.title('Simple Keyword Extractor')
-
-# Custom CSS to make content use full width
-st.markdown("""
-<style>
-    .block-container {
-        padding: 2rem;
-        max-width: 100%;
-    }
-    .stContainer {
-        width: 100%;
-    }
-    .css-1d391kg {
-        background-color: #90e0ef;
-    }
-    .stApp {
-        background-color: #ffb703;
-    }
-    .stTextArea textarea {
-        background-color: #e0e0e0;  /* Background color */
-        color: #333333;  /* Text color */
-        font-size: 16px;  /* Font size */
-        border: 2px solid #cccccc;  /* Border style */
-        border-radius: 5px;  /* Rounded corners */
-        padding: 10px;  /* Padding inside the text area */
-    }        
-</style>
+# Custom CSS to style the app
+st.markdown(f"""
+    <style>
+        .block-container {{
+            padding-top: 2rem;
+            padding-bottom: 2rem;
+            padding-left: 2rem;
+            padding-right: 2rem;
+            background-color: #b6a081;
+            border-radius: 10px;
+            border: 1px solid #F0C775;
+            background: #B6A081;
+            backdrop-filter: blur(2px);
+            width: 80%;
+            margin: auto;
+        }}
+        .stApp {{
+            background-color: #284867;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }}
+        .stTextArea textarea {{
+            background-color: #f5f5dc;
+            color: #333333;
+            font-size: 16px;
+            border: 2px solid #cccccc;
+            border-radius: 10px;
+            padding: 10px;
+        }}
+        .stButton button {{
+            background-color: #284867;
+            color: white;
+            font-size: 16px;
+            border-radius: 5px;
+            padding: 10px 20px;
+            transition: background-color 0.3s;
+        }}
+        .stButton button:active {{
+            background-color: #f0c775;
+        }}
+        .css-1aumxhk, .css-1v0mbdj, .css-1d391kg {{
+            margin-bottom: 20px;
+        }}
+        .center-logo {{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 20px;
+        }}
+        .logo-img {{
+            max-width: 200px;
+        }}
+        h1.title {{
+            margin-top: -30px;
+            text-align: center;
+            color: #4a4a4a;
+        }}
+        .expander-header {{
+            color: #4a4a4a !important;
+        }}
+        .expander-content {{
+            display: none; /* Hide expander content by default */
+        }}
+    </style>
+    <div class="center-logo">
+        <img class="logo-img" src="data:image/png;base64,{logo_base64}" />
+    </div>
+    <h1 class="title">Simple Keyword Extractor</h1>
 """, unsafe_allow_html=True)
 
 # Create two columns for layout of equal width
@@ -67,17 +115,18 @@ col1, col2 = st.columns(2)
 
 # Left column for user input and instructions
 with col1:
-    user_input = st.text_area("Enter text here:", height=250)
-    if st.button("Sumbit"): 
+    user_input = st.text_area("Enter text here", height=250)
+    submit_button = st.button("Submit")
+    if submit_button:
         st.write("You have submitted the text. Please wait for the results...")
 
-    with st.expander("How to Use This Tool"):
-        st.write("""
+    with st.expander("How to Use This Tool", expanded=False):
+        st.markdown("""
         - **Step 1:** Paste or type your text into the text area above.
         - **Step 2:** Press Enter or click outside the text box to process the text.
         - **Step 3:** View the extracted keywords and their importance scores below the text box.
         - **Step 4:** If installed, view the optional word cloud visualizing keyword importance.
-        """)
+        """, unsafe_allow_html=True)
 
 # Right column for displaying results
 with col2:
